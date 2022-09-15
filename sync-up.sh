98d0a826c25a61f44dfa0e09ebf5d2f5df2f4785
Matej Jezdinsky <matejjezdinsky@gmail.com>
2022-09-15 10:53:44 +0200

    Transport database and leave backup on the server

--- sync-up.sh
+++ sync-up.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/bash
 
+#many script taken directly from https://github.com/CloudRaker/tfp-heartland/blob/master/scripts/sync-up
+
 if [ ! -f ".env" ]
 then
    echo ".env file not found!"
@@ -8,4 +10,29 @@ fi
 
 source ./.env
 
-echo "-$SERVER_DB-"
+## copy local database
+stamp=$(date +"%Y-%m-%d-%H%M")
+filename=$LOCAL_DB-$stamp.mongodump
+
+### use --gzip
+
+# db.createUser( { user: "myuser", pwd: "password", roles: ["readWrite"] })
+mongodump -d $LOCAL_DB --archive=./$filename
+
+SSH_PORT=22
+remoteSSH="-p $SSH_PORT $SERVER_ROOT"
+rsyncTransport="ssh -p $SSH_PORT"
+rsyncDestination="$SERVER_ROOT"
+
+## move and remove archive
+rsync -av -e "$rsyncTransport" ./$filename $rsyncDestination:$SERVER_PATH/$filename
+rm -rf ./$filename
+
+## backup remote copy
+ssh $remoteSSH "mongodump --uri=$SERVER_DB --archive >> $SERVER_PATH/$filename.bak"
+
+## apply new data
+ssh $remoteSSH "mongorestore --username=$SERVER_DB_USER --password=$SERVER_DB_PASS --noIndexRestore --drop --nsInclude=$LOCAL_DB.* --nsFrom=$LOCAL_DB.* --nsTo=$SERVER_DB_NAME.* --archive=$SERVER_PATH/$filename"
+
+## remove carried data stuff
+ssh $remoteSSH rm -rf $SERVER_PATH/$filename
