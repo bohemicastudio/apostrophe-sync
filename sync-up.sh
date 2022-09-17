9dd018bb2ddc31897817c55850596ad778cf3633
Matej Jezdinsky <matejjezdinsky@gmail.com>
2022-09-17 11:27:29 +0200

    Fixup the sync-up script

--- sync-up.sh
+++ sync-up.sh
@@ -11,20 +11,22 @@ source ./.env
 stamp=$(date +"%Y-%m-%d-%H%M")
 filename=$LOCAL_DB-$stamp.mongodump
 
-remote_ssh="-p $SERVER_SSH_PORT $SERVER"
 server_ssh="$SERVER_USER@$SERVER_IP"
+remote_ssh="-p $SERVER_SSH_PORT $server_ssh"
 server_uri="mongodb://$SERVER_DB_USER:$SERVER_DB_PASS@$SERVER_DB_NAME:27017/$server?$SERVER_DB_EXTRA"
 
 
 # Create local archive
 mongodump -d $LOCAL_DB --archive=./$filename
 
-# Transport archive and remove local
+# Transport archive
 rsync -av -e "ssh -p $SERVER_SSH_PORT" ./$filename $server_ssh:$SERVER_PATH/$filename
+
+# Remove the local file
 rm -rf ./$filename
 
 # Backup remote copy
-ssh $remote_ssh "mongodump --uri=$server_uri --archive >> $SERVER_PATH/$filename.bak"
+ssh $remote_ssh "mongodump --uri=$server_uri --archive >> $SERVER_PATH/$SERVER_DB_NAME-$stamp.mongodump.bak"
 
 # Apply local data to remote
 ssh $remote_ssh "mongorestore --username=$SERVER_DB_USER --password=$SERVER_DB_PASS --noIndexRestore --drop --nsInclude=$LOCAL_DB.* --nsFrom=$LOCAL_DB.* --nsTo=$SERVER_DB_NAME.* --archive=$SERVER_PATH/$filename"
