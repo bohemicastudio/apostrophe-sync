352db17d0b22550d76f2e0f07463501c28f550e4
Matej Jezdinsky <matejjezdinsky@gmail.com>
2022-10-12 00:13:46 +0200

    Reduce spaghetti code in main script

--- aposync.sh
+++ aposync.sh
@@ -4,6 +4,8 @@
 scriptdir="$(dirname "$0")"
 source $scriptdir/.shared.sh
 
+
+## Global variables
 gopass=0
 
 
@@ -26,7 +28,7 @@ elif [ $# -eq 1 ]; then
   type="$1"
 
 else
-  echo -e "${Bold_On}:: Uploads (server ↑):${Styling_Off}"
+  echo -e "${Bold_On}:: Upload actions (server ↑):${Styling_Off}"
   echo -e "   [${Underline_On}1${Styling_Off}] Sync up database && files"
   echo -e "       ↳ [${Underline_On}10${Styling_Off}] Sync up database"
   # echo -e "              ↳ [${Underline_On}100${Styling_Off}] Restore from file on server (TODO)"
@@ -35,7 +37,7 @@ else
   echo -e "              ↳ [${Underline_On}110${Styling_Off}] Sync up files - preview"
   echo -e "              ↳ [${Underline_On}111${Styling_Off}] Sync up files - force delete"
   echo -e ""
-  echo -e "${Bold_On}:: Downloads (local ↓):${Styling_Off}"
+  echo -e "${Bold_On}:: Download actions (local ↓):${Styling_Off}"
   echo -e "   [${Underline_On}2${Styling_Off}] Sync down database && files"
   echo -e "       ↳ [${Underline_On}20${Styling_Off}] Sync down database"
   # echo -e "              ↳ [${Underline_On}200${Styling_Off}] Restore from file on local (TODO)"
@@ -49,104 +51,67 @@ fi
 
 ## Run the script
 
-if [ $type -eq 1 ]; then
-  echoTitle "Sync up database && files"
+Verify () {
+  echoTitle "$1"
+
   if [ $gopass == 0 ]; then
     echoCmd "Run the command? [Yes/No]"
     read go
   fi
+
   if [ $gopass == 1 ] || [ "$go" == "y" ] || [ "$go" == "yes" ] || [ "$go" == "Y" ] || [ "$go" == "Yes" ] || [ "$go" == "YES" ]; then
+    return 0
+  else
+    echoCmd "Action prevented"
+    return 1
+  fi
+}
+
+
+if [ $type -eq 1 ]; then
+  if Verify "Sync up database && files"; then
     $scriptdir/sync-up.sh && $scriptdir/files-up.sh && exit 0
   fi
 
-
 elif [ $type -eq 10 ]; then
-  echoTitle "Sync up database"
-  if [ $gopass == 0 ]; then
-    echoCmd "Run the command? [Yes/No]"
-    read go
-  fi
-  if [ $gopass == 1 ] || [ "$go" == "y" ] || [ "$go" == "yes" ] || [ "$go" == "Y" ] || [ "$go" == "Yes" ] || [ "$go" == "YES" ]; then
+  if Verify "Sync up database"; then
     $scriptdir/sync-up.sh && exit 0
   fi
 
-
 elif [ $type -eq 11 ]; then
-  echoTitle "Sync up files"
-  if [ $gopass == 0 ]; then
-    echoCmd "Run the command? [Yes/No]"
-    read go
-  fi
-  if [ $gopass == 1 ] || [ "$go" == "y" ] || [ "$go" == "yes" ] || [ "$go" == "Y" ] || [ "$go" == "Yes" ] || [ "$go" == "YES" ]; then
+  if Verify "Sync up files"; then
     $scriptdir/files-up.sh && exit 0
   fi
 
-
 elif [ $type -eq 110 ]; then
-  echoTitle "Sync up files - preview"
-  if [ $gopass == 0 ]; then
-    echoCmd "Run the command? [Yes/No]"
-    read go
-  fi
-  if [ $gopass == 1 ] || [ "$go" == "y" ] || [ "$go" == "yes" ] || [ "$go" == "Y" ] || [ "$go" == "Yes" ] || [ "$go" == "YES" ]; then
+  if Verify "Sync up files - preview"; then
     $scriptdir/files-up.sh -d && exit 0
   fi
 
-
 elif [ $type -eq 111 ]; then
-  echoTitle "Sync up files - forced delete"
-  if [ $gopass == 0 ]; then
-    echoCmd "Run the command? [Yes/No]"
-    read go
-  fi
-  if [ $gopass == 1 ] || [ "$go" == "y" ] || [ "$go" == "yes" ] || [ "$go" == "Y" ] || [ "$go" == "Yes" ] || [ "$go" == "YES" ]; then
+  if Verify "Sync up files - forced delete"; then
     $scriptdir/files-up.sh -f && exit 0
   fi
 
-
 elif [ $type -eq 2 ]; then
-  echoTitle "Sync down database && files"
-  if [ $gopass == 0 ]; then
-    echoCmd "Run the command? [Yes/No]"
-    read go
-  fi
-  if [ $gopass == 1 ] || [ "$go" == "y" ] || [ "$go" == "yes" ] || [ "$go" == "Y" ] || [ "$go" == "Yes" ] || [ "$go" == "YES" ]; then
+  if Verify "Sync down database && files"; then
     $scriptdir/sync-down.sh && $scriptdir/files-down.sh && exit 0
   fi
 
-
 elif [ $type -eq 20 ]; then
-  echoTitle "Sync down database"
-  if [ $gopass == 0 ]; then
-    echoCmd "Run the command? [Yes/No]"
-    read go
-  fi
-  if [ $gopass == 1 ] || [ "$go" == "y" ] || [ "$go" == "yes" ] || [ "$go" == "Y" ] || [ "$go" == "Yes" ] || [ "$go" == "YES" ]; then
+  if Verify "Sync down database"; then
     $scriptdir/sync-down.sh && exit 0
   fi
 
-
 elif [ $type -eq 21 ]; then
-  echoTitle "Sync down files"
-  if [ $gopass == 0 ]; then
-    echoCmd "Run the command? [Yes/No]"
-    read go
-  fi
-  if [ $gopass == 1 ] || [ "$go" == "y" ] || [ "$go" == "yes" ] || [ "$go" == "Y" ] || [ "$go" == "Yes" ] || [ "$go" == "YES" ]; then
+  if Verify "Sync down files"; then
     $scriptdir/files-down.sh && exit 0
   fi
 
-
 elif [ $type -eq 210 ]; then
-  echoTitle "Sync down files - preview"
-  if [ $gopass == 0 ]; then
-    echoCmd "Run the command? [Yes/No]"
-    read go
-  fi
-  if [ $gopass == 1 ] || [ "$go" == "y" ] || [ "$go" == "yes" ] || [ "$go" == "Y" ] || [ "$go" == "Yes" ] || [ "$go" == "YES" ]; then
+  if Verify "Sync down files - preview"; then
     $scriptdir/files-down.sh -d && exit 0
   fi
-
 
 else
   echoAlert "Unsupported action"
