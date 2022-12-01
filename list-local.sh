fe5595181c6feb407d1383891a1455eb1f9f904d
Matej Jezdinsky <matejjezdinsky@gmail.com>
2022-12-01 17:37:41 +0100

    New print-out tables

--- list-local.sh
+++ list-local.sh
@@ -15,15 +15,22 @@ fi
 ## Run the script
 
 # List all available snapshots in some pretty format
-available=$(ls $LOCAL_MONGO_BACKUPS_FOLDER_PATH)
+available=$(ls -lh $LOCAL_MONGO_BACKUPS_FOLDER_PATH)
 
+IFS=$'\n'
 array=($available)
 len=${#array[@]}
+IFS=$' '
 
-printf "${Underline_White}Date       Time     | Database                 (user)${Styling_Off}\n"
-for (( j=0; j<"$len"; j++ ))
+printf "${Underline_White}Date       Time     | Database                     | Size     | File etx.      (user)${Styling_Off}\n"
+for (( j=1; j<"$len"; j++ ))
 do
-  echo ${array[$j]}_$j | awk -F'_' '{ sub("-", ":", $3); sub("-", ":", $3); printf "%s %s | %24s (%s)\n", $2,$3,$1,$4 }' | sed 's/.mongodump//g' | sed 's/.bak//g'
+  echo ${array[$j]} $j \
+      | awk -F' ' '{ printf "%s_%s_%s\n", $10,$5,$9 }' \
+      | awk -F'_' '{ printf "%s %s\n", $0,$6; }' \
+      | sed 's/_/ /g' \
+      | awk -F' ' '{ split($6,a,"."); $6=a[1]; sub(a[1], "", $7); sub("-",":",$5); sub("-",":",$5); printf "%s\n", $0; }' \
+      | awk -F' ' '{ printf "%s %s | %-28s | %8s | %-14s (%s)\n", $4,$5,$3,$2,$7,$6 }'
 done
 
 
